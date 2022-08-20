180d003c9ecc492571636bde32225bdf9ae4d049
AroneSusau <arone.s@live.com.au>
2022-08-21 08:39:17 +1000

    adding google analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -19,6 +20,20 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap"
           rel="stylesheet"
         ></link>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-M4087ERKM6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-M4087ERKM6');
+        `}
+        </Script>
       </Head>
       <body style={{ margin: 0, backgroundColor: '#000000' }}>
         <Main />
